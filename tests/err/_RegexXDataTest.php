0039096cbead7b714df5fe7d6b8f9e7d96f0b45b
dougw <dougwilbourne@gmail.com>
2023-07-14 11:38:46 -0400

    minor update to phpunit.xml. removed unnecessary individual tests of exceptions

--- tests/err/_RegexXDataTest.php
+++ tests/err/_RegexXDataTest.php
@@ -17,9 +17,12 @@ use pvc\regex\err\_RegexXData;
 class _RegexXDataTest  extends XDataTestMaster
 {
     /**
-     * testPvcRegexExceptionLibrary
+     * @function testPvcRegexExceptionLibrary
      * @covers \pvc\regex\err\_RegexXData::getXMessageTemplates
      * @covers \pvc\regex\err\_RegexXData::getLocalXCodes
+     * @covers \pvc\regex\err\RegexBadPatternException::__construct
+     * @covers \pvc\regex\err\RegexInvalidDelimiterException::__construct
+     * @covers \pvc\regex\err\RegexInvalidMatchIndexException::__construct
      */
     public function testPvcRegexExceptionLibrary(): void
     {
